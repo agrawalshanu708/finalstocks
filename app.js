79ffc43f1efb423f48920502ae175de49e44089d
shanu agrawal <agrawalshanu798@gmail.com>
2021-12-02 09:51:46 +0530

    updated

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var entryPrice = document.querySelector("#entry-price")
-var noOfStocks = document.querySelector("#no-of-stocks")
-var currentPrice = document.querySelector("#current-price")
-var checkButton = document.querySelector("#check-button")
-var outputStatus = document.querySelector("#output-status")
+const entryPrice = document.querySelector("#entry-price")
+const noOfStocks = document.querySelector("#no-of-stocks")
+const currentPrice = document.querySelector("#current-price")
+const checkButton = document.querySelector("#check-button")
+const outputStatus = document.querySelector("#output-status")
 
 checkButton.addEventListener("click", function clickValidate(){
-    var net = (currentPrice.value - entryPrice.value)*noOfStocks.value ;
-    var x = entryPrice.value*noOfStocks.value;
-    var per = (net/x)*100;
+    let net = (currentPrice.value - entryPrice.value)*noOfStocks.value ;
+    let x = entryPrice.value*noOfStocks.value;
+    let per = (net/x)*100;
     if( net >=0 ){
         outputStatus.innerText = ("your net profit  and percentage are " +net+ " and " +per+ "%")
        
